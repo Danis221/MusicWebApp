3ca92e47ac95bbb9be272879fcf17f58d45ea742
DESKTOP-FIBDM79\danis <danisza4@gmail.com>
2022-10-14 10:46:12 +0300

    added characterFilter

---
+++ MusicWebApp/src/main/java/ru/itis/filter/CharacterFilter.java
@@ -0,0 +1,26 @@
+package ru.itis.filter;
+
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(filterName = "characterFilter", urlPatterns = "/*")
+public class CharacterFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletRequest.setCharacterEncoding("UTF-8");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
