f1e22c3fc519f0e398297c803671aa4c399e45dd
DESKTOP-FIBDM79\danis <danisza4@gmail.com>
2022-10-18 23:23:57 +0300

    add validator for name and lastname

--- MusicWebApp/src/main/webapp/js/validatorForAuth.js
+++ MusicWebApp/src/main/webapp/js/validatorForAuth.js
@@ -5,7 +5,7 @@ const firstname = document.getElementById("firstname");
 const form = document.querySelector("form");
 const errorMessage = document.getElementById("errorMessage");
 const passw = /(?=.*[0-9]){6,}/g;
-
+const name = /^[a-zA-Zа-яА-Я'][a-zA-Zа-яА-Я-' ]+[a-zA-Zа-яА-Я']?$/u;
 form.addEventListener("submit", (e) => {
     const errors = [];
 
@@ -22,6 +22,14 @@ form.addEventListener("submit", (e) => {
     }
     if (!password.value.match(passw)) {
         errors.push("password is too easy")
+    }
+
+    if(!username.value.match(name)) {
+        errors.push("firstname is too weird")
+    }
+
+    if(!lastname.value.match(name) ) {
+        errors.push("lastname is too weird")
     }
 
     if (errors.length > 0) {
