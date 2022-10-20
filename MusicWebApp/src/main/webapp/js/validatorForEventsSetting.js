d26b674e4cf7f383ef3715d489602cd603ad6201
DESKTOP-FIBDM79\danis <danisza4@gmail.com>
2022-10-20 20:24:05 +0300

    fix price

--- MusicWebApp/src/main/webapp/js/validatorForEventsSetting.js
+++ MusicWebApp/src/main/webapp/js/validatorForEventsSetting.js
@@ -2,15 +2,11 @@ const startDate = document.getElementById("startDate");
 const endDate = document.getElementById("endDate");
 const price = document.getElementById("price");
 const form = document.querySelector("form");
-const regNumber = / ^ [0-9] * [1-9] [0-9] * $ /;
 const errorMessage = document.getElementById("errorMessage");
 
 form.addEventListener("submit", e => {
     const errors = [];
 
-    if(!price.value.match(regNumber)) {
-        errors.push("is it a positive number?");
-    }
 
     if (startDate.value !== "" && endDate.value === "") {
         errors.push("you did not enter a start date");
